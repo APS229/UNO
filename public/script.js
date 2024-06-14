f905070faacc335849d4932ea6129dfaa6302748
APS229 <apsofficial229@gmail.com>
2024-06-14 15:40:51 +0530

    Add UNO cards client side

--- public/script.js
+++ public/script.js
@@ -1,4 +1,35 @@
 window.onload = () => {
+    // ordered according to texture atlas
+    const COLORS = ['red', 'yellow', 'green', 'blue'];
+    // size of each card
+    const tileWidth = 240;
+    const tileHeight = 360;
+
+    const cards = {};
+
+    let j = 0;
+    for (const color of COLORS) {
+        cards[color] = {};
+        for (let i = 0; i <= 9; i++) {
+            cards[color][i] = { x: i, y: j};
+        }
+        cards[color].skip = { x: 10, y: j};
+        cards[color].reverse = { x: 11, y: j};
+        cards[color]['+2'] = { x: 12, y: j};
+        j++;
+    }
+    cards['wild'] = {
+        '0': { x: 13,  y: 0 },
+        '+4': { x: 13, y: 4 }
+    };
+    function getPositionByCard(card) {
+        let [cardType, cardValue] = card.split(' ');
+        if (cardType === 'wild' && cardValue !== '+4') cardValue = '0';
+        if (!cards[cardType] || !cards[cardType][cardValue]) return '0px 0px';
+        console.log(card);
+        return ('-' + cards[cardType][cardValue].x * tileWidth + 'px') + ' ' + ('-' + cards[cardType][cardValue].y * tileHeight + 'px');
+    }
+
     let players = {};
 
     const connect = document.getElementById('connect');
@@ -13,7 +44,7 @@ window.onload = () => {
             document.getElementById('playerinfo').style.display = 'none';
             document.getElementById('disconnect').style.display = 'none';
             document.getElementById('start').style.display = 'block';
-            document.getElementById('players').style.display = 'flex';
+            document.getElementById('game').style.display = 'block';
 
             socket.emit('username', username);
 
@@ -36,7 +67,7 @@ window.onload = () => {
                     const playerElement = document.createElement('div');
                     playerElement.id = p;
                     playerElement.setAttribute('class', 'player');
-                    playerElement.innerHTML = `<p>${player.username}</p><b>${players[p].cardsLength}</b>`;
+                    playerElement.innerHTML = `<b>${player.username}</b><p>${players[p].cardsLength} cards left</p>`;
                     document.getElementById('players').append(playerElement);
                 }
             });
@@ -48,7 +79,7 @@ window.onload = () => {
                 playerElement.id = player.id;
                 playerElement.setAttribute('class', 'player');
                 playerElement.innerText = player.username;
-                playerElement.innerHTML = `<p>${player.username}` + (player.id === socket.id ? '<a> (You)</a>' : '') + `</p><b>${players[player.id].cardsLength}</b>`;
+                playerElement.innerHTML = `<b>${player.username}` + (player.id === socket.id ? '<a> (You)</a>' : '') + `</b><p>${players[player.id].cardsLength} cards left</p>`;
                 document.getElementById('players').append(playerElement);
             });
 
@@ -60,13 +91,18 @@ window.onload = () => {
             socket.on('start', () => {
                 document.getElementById('start').style.display = 'none';
             });
+
+            socket.on('topCard', card => {
+                document.getElementById('card').style.background = "url('./textures/cards.png')";
+                document.getElementById('card').style.backgroundPosition = getPositionByCard(card);
+            });
         });
 
         // display disconnected title, stop displaying the game
         socket.on('disconnect', () => {
             document.getElementById('disconnect').style.display = 'block';
             document.getElementById('start').style.display = 'none';
-            document.getElementById('players').style.display = 'none';
+            document.getElementById('game').style.display = 'none';
             document.getElementById('players').innerHTML = '';
         });
     }
